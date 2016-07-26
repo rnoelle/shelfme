60b4da66eadd392c1d17c49fe28bb21d7e155c01
rnoelle <reidnoelle2@gmail.com>
2016-07-25 20:28:09 -0600

    trying

--- js/routes.js
+++ js/routes.js
@@ -4,22 +4,22 @@ angular.module('shelfme')
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: '/js/views/home/homeTmpl.html',
+        templateUrl: 'js/views/home/homeTmpl.html',
         controller: 'homeCtrl'
       })
       .state('search', {
         url: '/search',
-        templateUrl: '/js/views/search/searchTmpl.html',
+        templateUrl: 'js/views/search/searchTmpl.html',
         controller: 'searchCtrl'
       })
       .state('recommend', {
         url: '/recommend/:bookId',
-        templateUrl: '/js/views/rec/recTmpl.html',
+        templateUrl: 'js/views/rec/recTmpl.html',
         controller: 'recCtrl'
       })
       .state('myShelf', {
         url: '/my-shelf',
-        templateUrl: '/js/views/myShelf/shelfTmpl.html',
+        templateUrl: 'js/views/myShelf/shelfTmpl.html',
         controller: 'shelfCtrl'
       });
 
--- js/views/rec/recCtrl.js
+++ js/views/rec/recCtrl.js
@@ -6,17 +6,23 @@ angular.module('shelfme')
     if($stateParams) {
       dataService.getBook(book).then(function (response) {
         $scope.baseBook = response;
+        if(response.volumeInfo.categories) {
           dataService.searchGenre(response.volumeInfo.categories[0]).then(function (response) {
             if (response.items) {
-            $scope.recommendations = response.items;
-          } else {
-            $scope.recommendations = [];
-            $scope.recommendations[0] = {};
-            $scope.recommendations[0].volumeInfo = {};
-            $scope.recommendations[0].volumeInfo.title = "Sorry, no recommendations found.";
-          }
-          console.log($scope.recommendations);
-        });
+              $scope.recommendations = response.items;
+            } else {
+              $scope.recommendations = [];
+              $scope.recommendations[0] = {};
+              $scope.recommendations[0].volumeInfo = {};
+              $scope.recommendations[0].volumeInfo.title = "Sorry, no recommendations found.";
+            }
+          });
+        } else {
+          $scope.recommendations = [];
+          $scope.recommendations[0] = {};
+          $scope.recommendations[0].volumeInfo = {};
+          $scope.recommendations[0].volumeInfo.title = "Sorry, no recommendations found.";
+        }
           });
       }
 

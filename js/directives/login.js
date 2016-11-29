cc084137f68928ea4626e51858874d70a7f57d26
rnoelle <reidnoelle2@gmail.com>
2016-11-29 15:54:16 -0700

    auth works, need to fix ui response

---
+++ js/directives/login.js
@@ -0,0 +1,40 @@
+angular.module('shelfme')
+  .directive('login', function() {
+    return {
+      restrict: 'E',
+      templateUrl: './js/directives/login.html',
+      link: function(scope, element, attr) {
+        if (firebase.auth().currentUser) {
+          var user = firebase.auth().currentUser;
+          console.log('already logged in');
+          element.find('div').html('<img src="' + user.$.src.photoURL + '">');
+        }
+
+        element.on('click', function() {
+          var provider = new firebase.auth.GoogleAuthProvider();
+          console.log(firebase.auth().currentUser);
+
+          if (!firebase.auth().currentUser) {
+            console.log('logging in');
+            firebase.auth().signInWithPopup(provider).then(function(result) {
+              // This gives you a Google Access Token. You can use it to access the Google API.
+              var token = result.credential.accessToken;
+              // The signed-in user info.
+              var user = result.user;
+              element.find('div').html('<img src="' + user.$.src.photoURL + '">');
+              scope.dropdown = 'dropdown';
+            }).catch(function(error) {
+              // Handle Errors here.
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              // The email of the user's account used.
+              var email = error.email;
+              // The firebase.auth.AuthCredential type that was used.
+              var credential = error.credential;
+              console.log(errorMessage);
+            });
+          }
+        })
+      }
+    }
+  })
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,4 +1,10 @@
 angular.module('shelfme')
-  .controller('mainCtrl', function ($scope, dataService) {
-  
+  .controller('mainCtrl', function ($scope, $state, dataService) {
+    $scope.login = "Login";
+    $scope.logOut = function () {
+      console.log('logging out');
+      firebase.auth().signOut().then(function () {
+        $('login').html('<div><h3>Login</h3></div>')
+      })
+    }
   });
